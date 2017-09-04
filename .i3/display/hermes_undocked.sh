2275b639345eb68bec0f2a7c29c7543635bf4395
Pete Travis <pete.travis@rackspace.com>
2017-09-04 12:20:56 -0500

    new desk layout

--- .i3/display/hermes_undocked.sh
+++ .i3/display/hermes_undocked.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # layout file for an undocked laptop
-xrandr --output eDP1 --auto --primary
+xrandr --output eDP1 --off 
 xrandr --output DP1-1 --off 
 xrandr --output DP1-2 --off
+xrandr --output eDP1 --auto --primary
